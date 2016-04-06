011b4dc1fc470412bdc3419935125b2d588734ad
MengCheng Ko <ko88201@gmail.com>
2016-04-07 01:39:11 +0800

    Day06 換頁功能
    我學習到：
    1. 上下頁設定與切換

---
+++ Day06-SeguesExample/SeguesExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeguesExample
+//
+//  Created by 柯孟成 on 2016/4/7.
+//  Copyright © 2016年 ko88201. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
